676901adbd1b8f3baee07a589aa18d738b128387
LoYiLun <37390225+LoYiLun@users.noreply.github.com>
2019-04-23 21:33:48 +0800

    20190423_2
    1.改一些檔名以及輸出測試
    2.解析度還需再調整

--- Interactive/Assets/Scripts/Menu/ForTouchImage.cs
+++ Interactive/Assets/Scripts/Menu/ForTouchImage.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI
-    ;
+using UnityEngine.UI;
 public class ForTouchImage : MonoBehaviour {
-
+    private Image _this;
+    private int set;
 	// Use this for initialization
 	void Start () {
-		
+        _this = this.GetComponent<Image>();
+        set = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if(_this.color.a >= 0.3f&& set == 0)
+        {
+            _this.color -= new Color(0,0,0,0.005f);
+        }
+        else 
+        {
+            set = 1;
+            _this.color += new Color(0, 0, 0, 0.005f);
+            if (_this.color.a >= 1)
+                set = 0;
+        }
 	}
 }
